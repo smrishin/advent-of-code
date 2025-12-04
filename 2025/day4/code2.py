ea9103c4b268c77bb00985e2bd9ae5fce00d9670
Rishi <smrishin@gmail.com>
2025-12-04 02:41:58 -0500

    2025: day 4

---
+++ 2025/day4/code2.py
@@ -0,0 +1,41 @@
+grid = []
+
+with open('input.txt', 'r') as file:
+    for line in file:
+        # print(line.strip())
+        grid.append(list(line.strip()))
+
+for row in grid:
+    print(row, "\n")
+
+R = len(grid)
+C = len(grid[0])
+directions = [(0,1),(1,0),(0,-1),(-1,0),(1,1),(1,-1),(-1,1),(-1,-1)]
+res = 0
+
+while True:
+    removedThisRound = 0
+    for r in range(R):
+        for c in range(C):
+            if grid[r][c] == ".":
+                continue
+
+            count = 0
+            for ro, co in directions:
+                nr, nc = r + ro, c + co
+                if 0 <= nr < R and 0 <= nc < C and grid[nr][nc] == "@":
+                    count += 1
+                    if count > 3:
+                        break
+            if count > 3:
+                continue
+            else:
+                res += 1
+                grid[r][c] = "."
+                removedThisRound += 1
+    if removedThisRound == 0:
+        break
+                
+
+
+print(res)
